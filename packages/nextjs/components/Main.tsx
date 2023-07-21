5088e1f09b1546cf72d2205be332652ce5df0740
smarthug <kirklayer@gmail.com>
2023-07-22 00:04:50 +0200

    world button created

--- packages/nextjs/components/Main.tsx
+++ packages/nextjs/components/Main.tsx
@@ -1,9 +1,11 @@
-import { IDKitWidget, solidityEncode, internal } from '@worldcoin/idkit'
+import WorldButton from "./WorldButton";
+import { IDKitWidget, internal, solidityEncode } from "@worldcoin/idkit";
 
 export default function Main() {
   return (
     <>
       <h1>Main</h1>
+      <WorldButton />
     </>
   );
 }
