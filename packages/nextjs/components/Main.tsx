2e42125adf852e089b7e72939d87d1c6b671c21d
smarthug <kirklayer@gmail.com>
2023-07-22 17:23:37 +0200

    header init

--- packages/nextjs/components/Main.tsx
+++ packages/nextjs/components/Main.tsx
@@ -1,19 +1,9 @@
-import BiconomyButton from "./BiconomyButton";
-import FarmerCard from "./FarmerCard";
-import IPFSUploader from "./IPFSUploader";
-import Map from "./Map";
-import WorldButton from "./WorldButton";
+
 
 export default function Main() {
   return (
     <>
       <h1>Main</h1>
-      <WorldButton />
-      <BiconomyButton />
-      <IPFSUploader />
-      <Map />
-
-      <FarmerCard />
     </>
   );
 }
