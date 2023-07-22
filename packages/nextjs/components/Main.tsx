2b7599d230ca7a5c81cfba99a7a02d82c84e31e1
smarthug <kirklayer@gmail.com>
2023-07-22 23:36:14 +0200

    cherry picked walletConnectButton

--- packages/nextjs/components/Main.tsx
+++ packages/nextjs/components/Main.tsx
@@ -1,8 +1,10 @@
+import WalletConnectButton from "./WalletConnectButton";
 
 export default function Main() {
   return (
     <>
       <h1>Main</h1>
+      <WalletConnectButton />
     </>
   );
 }
