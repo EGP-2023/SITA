5251651b826bdceeb3b0d0aeddc71ba4b460811b
LiamFaruqDeko <liam.faruq@pay4later.com>
2023-07-22 10:45:25 +0100

    feature/refine-6551-account-registry-for-sita

---
+++ packages/hardhat/util/contract-names.ts
@@ -0,0 +1,8 @@
+enum ContractNames {
+    SITA_ACCOUNT = 'SitaAccount',
+    SITA_ACCOUNT_REGISTRY = 'SitaAccountRegistry',
+    AnotherContract = 'AnotherContract',
+    // Add more contract names here...
+  }
+  
+  export default ContractNames;
