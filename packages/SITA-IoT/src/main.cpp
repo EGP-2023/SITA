31e851bd7f78f5c6b973611b1ae91d8c3e95268b
chethack <techbuddy022@gmail.com>
2023-07-22 10:58:12 +0200

    added SHT10 sensor endpoint

--- packages/SITA-IoT/src/main.cpp
+++ packages/SITA-IoT/src/main.cpp
@@ -3,14 +3,17 @@
 #include <ESPAsyncWebServer.h>
 #include <ArduinoJson.h>
 #include <AsyncJson.h>
+#include <SHT1x-ESP.h>
 
 #define SSID "please"
 #define PWD "trialnetwork"
+#define dataPin 15
+#define clockPin 2
 
+SHT1x sht1x(dataPin, clockPin, SHT1x::Voltage::DC_3_3v);
 AsyncWebServer server(80);
 StaticJsonDocument<250> jsonDocument;
 char buffer[250];
-
 float temperature, humidity;
 
 void connectToWiFi() {
@@ -34,11 +37,26 @@ void hello_json(AsyncWebServerRequest *request) {
   request->send(200, "application/json", buffer);
 }
 
+void sensor_data(float temperature, float humidity) {
+  jsonDocument.clear();
+  jsonDocument["temperature"] = temperature;
+  jsonDocument["humidity"] = humidity;
+  serializeJson(jsonDocument, buffer);
+}
+
+void getEnv(AsyncWebServerRequest *request) {
+  temperature = sht1x.readTemperatureC();
+  humidity = sht1x.readHumidity();
+  sensor_data(temperature, humidity);
+  request->send(200, "application/json", buffer);
+}
+
 void setup_routes() {
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
     request->send(200, "application/json", "{\"message\":\"Welcome\"}");
   });
   server.on("/hello", HTTP_GET, hello_json);
+  server.on("/env", HTTP_GET, getEnv);
   server.begin();
 }
 
