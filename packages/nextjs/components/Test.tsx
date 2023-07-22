6e2846e0b5ee65360053f466020779a5efe510c8
smarthug <kirklayer@gmail.com>
2023-07-22 17:23:37 +0200

    header init

---
+++ packages/nextjs/components/Test.tsx
@@ -0,0 +1,19 @@
+import BiconomyButton from "./BiconomyButton";
+import FarmerCard from "./FarmerCard";
+import IPFSUploader from "./IPFSUploader";
+import Map from "./Map";
+import WorldButton from "./WorldButton";
+
+export default function Test() {
+  return (
+    <>
+      <h1>Test</h1>
+      <WorldButton />
+      <BiconomyButton />
+      <IPFSUploader />
+      <Map />
+
+      <FarmerCard />
+    </>
+  );
+}
