5f35223332cfdedcf060bd406f8dcee9390a9f22
swinecoder101 <liamfaruq@gmail.com>
2023-08-19 08:48:10 +0100

    fix-type-script-errors

--- packages/nextjs/utils/sitaStore.tsx
+++ packages/nextjs/utils/sitaStore.tsx
@@ -2,7 +2,14 @@ import { create } from "zustand";
 
 console.log(create);
 
-export const useSitaStore = create(set => ({
+export interface SitaStore {
+  auth: boolean;
+  setAuth: (auth: boolean) => void;
+  biconomySmartAccount: any;
+  setBiconomySmartAccount: (biconomySmartAccount: any) => void;
+}
+
+export const useSitaStore = create<SitaStore>(set => ({
   auth: true,
   setAuth: (auth: boolean) => set({ auth }),
   biconomySmartAccount: null,
